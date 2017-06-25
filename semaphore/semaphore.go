75012896336ca65beca93470521f6b37a30ecb24
Andrew Zenk <andrew@andrewzenk.com>
2017-06-25 13:26:10 -0500

    Rename Semaphore struct to SemaphoreData

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -8,36 +8,36 @@ import (
 	"time"
 )
 
-type Semaphore struct {
+type SemaphoreData struct {
 	Index uint64   `json:"-"`
 	Max   int      `json:"max"`
 	Holders map[string]int64  `json:"holders"`
 }
 
-func New(max int) *Semaphore {
-	s := new(Semaphore)
+func New(max int) *SemaphoreData {
+	s := new(SemaphoreData)
 	s.Max = max
 	s.Holders = make(map[string]int64, max)
 	return s
 }
 
-func (s Semaphore) String() string {
-	return fmt.Sprintf("Semaphore - index: %d, count: %d, max %d, holders: %s", s.Index, s.Count(), s.Max, s.Holders)
+func (s SemaphoreData) String() string {
+	return fmt.Sprintf("SemaphoreData - index: %d, count: %d, max %d, holders: %s", s.Index, s.Count(), s.Max, s.Holders)
 
 }
 
-func Load(in io.Reader) (*Semaphore, error) {
-	s := new(Semaphore)
+func Load(in io.Reader) (*SemaphoreData, error) {
+	s := new(SemaphoreData)
 	d := json.NewDecoder(in)
 	err := d.Decode(s)
 	return s, err
 }
 
-func (s *Semaphore)Count() int {
+func (s *SemaphoreData)Count() int {
 	return len(s.Holders)
 }
 
-func (s *Semaphore)ToJSON() (string, error) {
+func (s *SemaphoreData)ToJSON() (string, error) {
 	buf := new(bytes.Buffer)
 	e := json.NewEncoder(buf)
 	e.SetIndent("","")
@@ -45,7 +45,7 @@ func (s *Semaphore)ToJSON() (string, error) {
 	return buf.String(), err
 }
 
-func (s *Semaphore)Acquire(id string) bool {
+func (s *SemaphoreData)Acquire(id string) bool {
 	_, ok := s.Holders[id]
 	if ok {
 		return true
@@ -60,13 +60,13 @@ func (s *Semaphore)Acquire(id string) bool {
 }
 
 // Return true if id in list of holders, false otherwise
-func (s *Semaphore)Holds(id string) bool {
+func (s *SemaphoreData)Holds(id string) bool {
 	_,ok := s.Holders[id]
 	return ok
 }
 
 // Remove holder entry from semaphore if it's present
-func (s *Semaphore)Release(id string) bool {
+func (s *SemaphoreData)Release(id string) bool {
 	delete(s.Holders, id)
 	return !s.Holds(id)
 }
